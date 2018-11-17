6ee8d50f9d460d020e3339185c3315a9265d0a46
Luigi <thegaminggodlou@gmail.com>
2018-11-17 00:17:33 -0800

    Added popup and functions to transfer data into variables

--- src/js/app.js
+++ src/js/app.js
@@ -6,40 +6,90 @@
 
 const projects = [
   {
+    id: 1,
     name: "Tracy Harbour",
-    description: "This is a custom build interactive website for a restaurant located in Tracy, California. It includes a gallery, contact form, a fully searchable menu, and a clean and responsive layout. Coded in React.JS",
+    description: "Custom built interactive website. It includes a gallery, contact form, a fully searchable menu, and a clean and responsive layout.",
     skills: "React, Javascript, SASS, CSS, HTML, Docker, nginx",
-    screenshots: ["https://placeimg.com/800/800/tech"]
+    screenshots: ["https://placeimg.com/800/800/tech"],
+    livePreview: "https://www.tracyharbour.com"
   },
   {
-    name: "Tracy Harbour 2",
-    description: "This is a custom build interactive website for a restaurant located in Tracy, California. It includes a gallery, contact form, a fully searchable menu, and a clean and responsive layout. Coded in React.JS",
-    skills: "React, Javascript, SASS, CSS, HTML, Docker, nginx",
-    screenshots: ["https://placeimg.com/800/800/tech"]
+    id: 2,
+    name: "Milano Pizza",
+    description: "Custom built 1-page website, with a responsive menu, slideshow, and a backend menu editor.",
+    skills: "HTML, CSS, Javascript, jQuery, Bootstrap, SASS, PHP, MySQL",
+    screenshots: ["https://placeimg.com/800/800/tech"],
+    livePreview: "https://milanopizzatracyca.com/"
   },
   {
-    name: "Tracy Harbour 2",
-    description: "This is a custom build interactive website for a restaurant located in Tracy, California. It includes a gallery, contact form, a fully searchable menu, and a clean and responsive layout. Coded in React.JS",
-    skills: "React, Javascript, SASS, CSS, HTML, Docker, nginx",
+    id: 3,
+    name: "PROJECT: G-Tourneys",
+    description: "Project for a tournament software, included admin panel, user authentication, team organization, registration, support tickets, and responsive design. Project was decomissioned.",
+    skills: "Javascript, PHP, Bootstrap, HTML, CSS, MySQL, nginx",
     screenshots: ["https://placeimg.com/800/800/tech"]
   }
 ]
 let sortedProjects = [];
-let projectInfo = [];
+let projectInfo = {};
 const listProjectID = document.getElementById("list-projects");
 let sorted = false;
-listAllProjects = () => {
+let listAllProjects = () => {
     listProjectID.innerHTML = projects.map(function(project){
+
     let html = `<div class="col-md-4 col-sm-6 col-xs-12 mb-4">
               <div class="card text-white">
                 <img class="card-img" src="${project.screenshots[0]}" alt="Card image">
                 <div class="card-img-overlay card-hover">
                   <h5 class="card-title">${project.name}</h5>
                   <p class="card-text">${project.description}</p>
+                  <button type="button" class="btn btn-primary btn-lg btn-block" onclick="triggerPopup(${project.id})">Learn More</button>
                 </div>
               </div>
             </div>`;
     return html;
   }).join('')
 }
+let triggerPopup = (id) => {
+  popupProject = projects.find(project => project.id === id)
+  projectInfo = popupProject;
+  showPopup()
+}
+let showPopup = () => {
+  let modalHTML = `
+  <div class="modal fade" tabindex="-1" role="dialog" aria-hidden="true">
+    <div class="modal-dialog modal-lg modal-dialog-centered">
+      <div class="modal-content text-center">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLongTitle">${projectInfo.name}</h5>
+        </div>
+        <div class="modal-body">
+          <ul class="finished-stars">
+            **info**
+          </ul>
+          <button type="button" class="btn btn-primary btn-lg" onclick="resetGame()" data-dismiss="modal">Play again</button>
+        </div>
+      </div>
+    </div>
+  </div>
+  `;
+  $(modalHTML).modal();
+}
+let sortByTag = (t) => {
+  sorted = true;
+  // push the sorted results into its array
+  let filteredProjects = projects.filter(project => project.skills.includes(t));
+  listProjectID.innerHTML = filteredProjects.map(function(project){
+  let html = `<div class="col-md-4 col-sm-6 col-xs-12 mb-4">
+            <div class="card text-white">
+              <img class="card-img" src="${project.screenshots[0]}" alt="Card image">
+              <div class="card-img-overlay card-hover">
+                <h5 class="card-title">${project.name}</h5>
+                <p class="card-text">${project.description}</p>
+              </div>
+            </div>
+          </div>`;
+  return html;
+}).join('')
+}
+
 listAllProjects()
